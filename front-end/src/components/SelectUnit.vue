502f676441eafc82cc2b15c9067f0edc7152b68e
Brock37 <remondflorent@gmail.com>
2019-12-02 12:53:09 +0100

    create front-end folder for vueJS. Create with @vue/cli

---
+++ front-end/src/components/SelectUnit.vue
@@ -0,0 +1,90 @@
+<template lang="html">
+  <div>
+    <fieldset>
+      <legend>Unité</legend>
+      <table>
+        <tbody>
+          <tr>
+            <th>Faction </th>
+            <td>
+              <select @change="changeUnitSelector($event)" v-model="keyFaction" class="" name="id_faction" title="Sélectionnez une faction">
+                <option value="1">Nains</option>
+                <option value="2">Orques</option>
+                <option value="3">Hommes-bêtes</option>
+                <option value="4">Skavens</option>
+                </select>
+            </td>
+          </tr>
+          <tr>
+            <th>Rang </th>
+            <td>
+              <select @change="changeUnitSelector($event)" v-model="keyRang" class="" name="id_rang" title="Sélectionnez un rang pour l\'unité">
+                <option value="0">Héros</option>
+                <option value="1">Homme de main</option>
+                </select>
+            </td>
+          </tr>
+          <tr>
+            <th>Unité </th>
+            <td>
+              <select v-model="keyUnit" name="id_unite" title="Sélectionnez un rang pour l\'unité">
+                <option v-for="unit in units" :value="unit.id" v-bind:key="unit.id">{{unit.Nom}}</option>
+              </select>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </fieldset>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  data: function() {
+    return{
+      keyFaction: "",
+      keyRang: "",
+      keyUnit: "",
+      units:[] //Contient les unites disponible
+    }
+  },
+  methods :{
+    /*
+    Récupere la liste des unité disponible depuis l'API
+    en fonction de la faction et du rang choisis
+    */
+    changeUnitSelector() {
+      if (this.keyFaction !== "" && this.keyRang !== "")
+      {
+        //console.log("changement d'unités");
+        this.keyUnit = ""
+
+        var url= "http://127.0.0.1:3000/api/profile/"
+                + this.keyFaction + "/" + this.keyRang
+
+        axios.get(url).then(response =>{
+          this.units = response.data.reponse
+        })
+
+      }
+    }
+  },
+  watch : {
+    keyUnit: function (){
+      //Envoie un signal ainsi que la nouvelle valeur de keyUnit au parent
+      this.$emit('update:keyunit', this.keyUnit)
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+fieldset{
+  margin-left: auto;
+  margin-right: auto;
+  width: 50%;
+  min-width: 50%;
+  margin-bottom: 25px;
+}
+</style>
---
+++ front-end/src/views/test.vue
@@ -0,0 +1,60 @@
+<template lang="html">
+  <div class="main">
+    <h1>Mordheim Bands</h1>
+    
+    <SelectUnit v-bind:keyunit.sync="keyunit"/>
+    <ProfilUnit v-bind:key-unit="keyunit"/>
+  </div>
+
+</template>
+
+<script>
+import SelectUnit from '@/components/SelectUnit.vue'
+import ProfilUnit from '@/components/ProfilUnit.vue'
+export default {
+  name: 'test',
+  components: {
+    SelectUnit,
+    ProfilUnit
+  },
+  data: function(){
+    return{
+      keyunit: null
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+
+body{
+  margin: 0;
+  padding: 0;
+}
+
+h1{
+  text-align: center;
+}
+
+fieldset{
+  margin-left: auto;
+  margin-right: auto;
+  width: 50%;
+  min-width: 50%;
+  margin-bottom: 25px;
+}
+
+.profile table, .profile td{
+  border: 1px solid #333;
+}
+
+.profile td,th{
+  width: 50px;
+  text-align: center;
+}
+
+.profile thead{
+  background-color: #333;
+  color: #fff;
+}
+</style>
