94e9b2051ea1409e8d3032ca2c974dc805f58ae8
Brock37 <remondflorent@gmail.com>
2019-12-04 13:52:03 +0100

    Allow all method to consume API

--- back-end/index.js
+++ back-end/index.js
@@ -12,7 +12,7 @@ app.use(function (req, res, next){
   // Website you wish to allow to connect
   res.setHeader('Access-Control-Allow-Origin', '*');
   // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  res.setHeader('Access-Control-Allow-Methods', '*');
 
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
