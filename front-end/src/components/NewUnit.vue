63d1081e105645dbe31c43110a5b0cc7215b4758
Brock37 <remondflorent@gmail.com>
2019-12-04 13:51:39 +0100

    Create new Unit conponent

---
+++ front-end/src/components/NewUnit.vue
@@ -0,0 +1,71 @@
+<template lang="html">
+  <div class="popup createUnit" >
+    <input type="text" name="unit_name" placeholder="Nom de l'unité" v-model="newName">
+    <div class="layout">
+      <button type="button" name="valid" @click.prevent="validNewUnit()">Valider</button>
+      <button type="button" name="cancel" @click.prevent="closePopUp()">Annuler</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  props:[ 'profil' ],
+  data : function() {
+    return {
+      newName: "",
+      price: 0,
+      allUnit: [],
+      accepted: false
+    }
+  },
+  methods :{
+    closePopUp() {
+      this.$emit('closePopUp', this.lastIndex, this.accepted)
+    },
+    validNewUnit(){
+      if(this.newName !== ""){
+        var url= "http://127.0.0.1:3000/api/profile/"
+        var data = {
+          ID : this.lastIndex,
+          id_rang: this.profil.rang,
+          id_race: this.profil.faction,
+          Nom: this.newName,
+          Prix: this.price,
+          M : 0,
+          CC : 0,
+          CT : 0,
+          F : 0,
+          E : 0,
+          PV : 0,
+          I : 0,
+          A : 0,
+          Cd : 0,
+          Arme_Armure: "",
+          Regle: null
+        }
+        axios.post(url,data).then(()=> {
+          this.accepted= true
+          this.closePopUp()
+        })
+      }
+    }
+  },
+  computed : {
+    lastIndex(){
+      return this.allUnit[this.allUnit.length - 1 ].ID + 1
+    }
+  },
+  created: function(){
+    var url= "http://127.0.0.1:3000/api/profile/"
+    axios.get(url).then(response =>{
+      this.allUnit = response.data.reponse
+    })
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
